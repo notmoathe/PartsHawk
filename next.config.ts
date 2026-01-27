449283f717eded5c79488a4a6de351e4bdcbbb0e
notmoathe <moathebackup@gmail.com>
2026-01-26 22:16:53 -0600

    Fix: Config updates and verified build success

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  experimental: {
-    serverComponentsExternalPackages: ['puppeteer-core', '@sparticuz/chromium-min'],
-  },
+  /* config options here */
+  serverExternalPackages: ['puppeteer-core', '@sparticuz/chromium-min'],
 };
 
 export default nextConfig;
